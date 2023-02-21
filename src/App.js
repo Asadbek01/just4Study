8234509d6131bc25695b0d31d6b6a0cc20815bdc
Saidamir-Sh <saidamirpl@gmail.com>
2023-02-21 21:07:06 +0100

    created mui custom theme

--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
 
 import { BrowserRouter, Outlet } from 'react-router-dom';
+import { createTheme, ThemeProvider } from '@mui/material';
 import { Box } from '@mui/material';
 import { Navbar } from './Components';
 import { AppRoutes } from './routes';
 
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#1DB954'
+    },
+    background: {
+      main: '#191414',
+    },
+  }
+})
+
 function App() {
   return (
-    <BrowserRouter>
-    <Box sx={{ flexGrow: 1, backgroundColor: '#000' }}>
-      <Navbar />
-      <AppRoutes/>
-      <Outlet/>
-    </Box>
-    </BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+      <Box bgcolor="background.main" sx={{ flexGrow: 1 }}>
+        <Navbar />
+        <AppRoutes/>
+        <Outlet/>
+      </Box>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
